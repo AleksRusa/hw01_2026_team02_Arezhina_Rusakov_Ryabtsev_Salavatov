de539e0175afc2a6cd25ea9bca89bcb9f40354a3
Русаков Александр Андреевич <aleksandr27rusakov@gmail.com>
2026-03-04 15:24:57 +0300

    Add files via upload

---
+++ experiments_and_visualization/gyroscope_bottle_rolling_analysis.py
@@ -0,0 +1,46 @@
+import csv
+import matplotlib.pyplot as plt
+
+time = []
+gyro_x = []
+gyro_y = []
+gyro_z = []
+
+with open('gyroscope_bottle_rolling/RawData.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+
+    for row in reader:
+        time.append(float(row[0]))
+        gyro_x.append(float(row[1]))
+        gyro_y.append(float(row[2]))
+        gyro_z.append(float(row[3]))
+
+start_index = 0
+for i, t in enumerate(time):
+    if t >= 2.0:
+        start_index = i
+        break
+
+time = time[start_index:]
+gyro_x = gyro_x[start_index:]
+gyro_y = gyro_y[start_index:]
+gyro_z = gyro_z[start_index:]
+
+plt.figure(figsize=(10, 8))
+
+plt.subplot(3, 1, 1)
+plt.plot(time, gyro_x)
+plt.ylabel("Angular velocity X (rad/s)")
+
+plt.subplot(3, 1, 2)
+plt.plot(time, gyro_y)
+plt.ylabel("Angular velocity Y (rad/s)")
+
+plt.subplot(3, 1, 3)
+plt.plot(time, gyro_z)
+plt.xlabel("Time (s)")
+plt.ylabel("Angular velocity Z (rad/s)")
+
+plt.tight_layout()
+plt.show()
